980895e9e0ff0eb9394b4404e5ea2a4917a7e236
Antti Juustila <antti.juustila@gmail.com>
2018-12-13 11:53:06 +0200

    Fiddling with includes, set correct tab order to dialog.

--- BaseLayer/NetworkReader.cpp
+++ BaseLayer/NetworkReader.cpp
@@ -24,136 +24,134 @@
 
 
 namespace OHARBase {
-	
-	/**
-	 Constructor to create the reader with host name. See the
-	 constructor of Networker class about handling the parameters.
-	 @param hostName the host to reading data, including port number. In practise,
-	 this should always be local host for reader, i.e. 127.0.0.1 with port, eg. 127:0.0.1:2222.
-    @param obs The observer who gets notified of network events.
-    @param io_s The boost asio io service.
-	 */
-	NetworkReader::NetworkReader(const std::string & hostName,
-										  NetworkReaderObserver & obs,
-										  boost::asio::io_service & io_s)
-	:		Networker(hostName, io_s),
-			observer(obs),
-			TAG("NetReader ")
-	{
-      using namespace boost::asio::ip;
-		remote_endpoint = std::unique_ptr<udp::endpoint>(new boost::asio::ip::udp::endpoint(udp::v4(), port));
-		socket = std::unique_ptr<udp::socket>(new udp::socket(io_s, *remote_endpoint));
-	}
-	
-	/**
-	 Constructor to create the reader with host name. See the
-	 constructor of Networker class about handling the parameters.
-	 @param hostName the host to reading data. In practise,
-	 this should always be local host for reader, i.e. 127.0.0.1.
-	 @param portNumber The port to read data from.
-    @param obs The observer who gets notified of network events.
-    @param io_s The boost asio io service.
-	 */
-	NetworkReader::NetworkReader(const std::string & hostName,
-										  int portNumber,
-										  NetworkReaderObserver & obs,
-										  boost::asio::io_service & io_s)
-	:		Networker(hostName, portNumber, io_s),
-			observer(obs),
-			TAG("NetworkReader")
-	{
-      using namespace boost::asio::ip;
-		remote_endpoint = std::unique_ptr<udp::endpoint>(new udp::endpoint(udp::v4(), port));
-		socket = std::unique_ptr<udp::socket>(new udp::socket(io_s, *remote_endpoint));
-	}
-	
-	NetworkReader::~NetworkReader() {
-	}
-	
-	
-	
-	/** Starts the reader. If necessary, connects to the endpoint and then does
-	 asynchronous read. */
-	void NetworkReader::start() {
-		LOG(INFO) << TAG << "Start reading for data...";
-		running = true;
-		if (!socket->is_open()) {
-			LOG(INFO) << TAG << "Opening read socket";
-			socket->connect(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port));
-		}
-		buffer->fill(0);
-		socket->async_receive_from(boost::asio::buffer(*buffer),
-											*remote_endpoint,
-											boost::bind(&NetworkReader::handleReceive, this,
-															boost::asio::placeholders::error,
-															boost::asio::placeholders::bytes_transferred));
-		
-		LOG(INFO) << TAG << "Async recv ongoing";
-	}
-	
-	/** Handles the incoming data and possible errors. Places finally another read
-	 to the socket. 
-	 @param error Error code
-	 @param bytes_transferred How many bytes came in. */
-	void NetworkReader::handleReceive(const boost::system::error_code & error, std::size_t bytes_transferred) {
-		LOG(INFO) << TAG << "Async recv finished code: " << error.value() << " " << error.message();
-      LOG_IF(WARNING, (error != boost::system::errc::success)) << "Receive failed in reader " << error.value();
-		if (!error || error == boost::asio::error::message_size)
-		{
-			if (buffer->data()) {
-				std::string buf;
-				buf.resize(bytes_transferred);
-				buf.assign(buffer->begin(), bytes_transferred);
-            LOG(INFO) << TAG << "Received " << bytes_transferred << " bytes of data: " << buf;
-				if (buf.length()>0) {
-               nlohmann::json j = nlohmann::json::parse(buf);
-					Package p = j.get<OHARBase::Package>();
-//					if (p.parse(buf)) {
-               guard.lock();
-               msgQueue.push(p);
-               guard.unlock();
-               // And when data has been received, notify the observer.
-               observer.receivedData();
-//					}
-				}
-			} else {
-				LOG(WARNING) << TAG << "Async recv finished but NO data";
-			}
-			if (running)
-			{
-				start();
-			}
-		}
-	}
-	
-	/** Stops the reader by setting the running flag to false, effectively ending the thread
-	 loop in the threadFunc(). */
-	void NetworkReader::stop() {
-		LOG(INFO) << TAG << "Stop the reader...";
-		if (running) {
-			running = false;
-			LOG(INFO) << TAG << "Shutting down the socket.";
-			socket->cancel();
-			socket->close();
-		}
-	}
-	
-	/** Allows another object to read the package object received from the network.
-	 This method should be called by the NetworkReaderObserver only when it has been notified
-	 that data has arrived.
-	 @return The Package containing the data received from the previous ProcessorNode.
-	 */
-	Package NetworkReader::read() {
-		LOG(INFO) << TAG << "Reading results from reader";
-		guard.lock();
-		Package result;
-		if (!msgQueue.empty()) {
-			result = msgQueue.front();
-			msgQueue.pop();
-		}
-		guard.unlock();
-		return result;
-	}
-	
-
+    
+    /**
+     Constructor to create the reader with host name. See the
+     constructor of Networker class about handling the parameters.
+     @param hostName the host to reading data, including port number. In practise,
+     this should always be local host for reader, i.e. 127.0.0.1 with port, eg. 127:0.0.1:2222.
+     @param obs The observer who gets notified of network events.
+     @param io_s The boost asio io service.
+     */
+    NetworkReader::NetworkReader(const std::string & hostName,
+                                 NetworkReaderObserver & obs,
+                                 boost::asio::io_service & io_s)
+    :		Networker(hostName, io_s),
+    observer(obs),
+    TAG("NetReader ")
+    {
+        using namespace boost::asio::ip;
+        remote_endpoint = std::unique_ptr<udp::endpoint>(new boost::asio::ip::udp::endpoint(udp::v4(), port));
+        socket = std::unique_ptr<udp::socket>(new udp::socket(io_s, *remote_endpoint));
+    }
+    
+    /**
+     Constructor to create the reader with host name. See the
+     constructor of Networker class about handling the parameters.
+     @param hostName the host to reading data. In practise,
+     this should always be local host for reader, i.e. 127.0.0.1.
+     @param portNumber The port to read data from.
+     @param obs The observer who gets notified of network events.
+     @param io_s The boost asio io service.
+     */
+    NetworkReader::NetworkReader(const std::string & hostName,
+                                 int portNumber,
+                                 NetworkReaderObserver & obs,
+                                 boost::asio::io_service & io_s)
+    :		Networker(hostName, portNumber, io_s),
+    observer(obs),
+    TAG("NetworkReader")
+    {
+        using namespace boost::asio::ip;
+        remote_endpoint = std::unique_ptr<udp::endpoint>(new udp::endpoint(udp::v4(), port));
+        socket = std::unique_ptr<udp::socket>(new udp::socket(io_s, *remote_endpoint));
+    }
+    
+    NetworkReader::~NetworkReader() {
+    }
+    
+    
+    
+    /** Starts the reader. If necessary, connects to the endpoint and then does
+     asynchronous read. */
+    void NetworkReader::start() {
+        LOG(INFO) << TAG << "Start reading for data...";
+        running = true;
+        if (!socket->is_open()) {
+            LOG(INFO) << TAG << "Opening read socket";
+            socket->connect(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port));
+        }
+        buffer->fill(0);
+        socket->async_receive_from(boost::asio::buffer(*buffer),
+                                   *remote_endpoint,
+                                   boost::bind(&NetworkReader::handleReceive, this,
+                                               boost::asio::placeholders::error,
+                                               boost::asio::placeholders::bytes_transferred));
+        
+        LOG(INFO) << TAG << "Async recv ongoing";
+    }
+    
+    /** Handles the incoming data and possible errors. Places finally another read
+     to the socket.
+     @param error Error code
+     @param bytes_transferred How many bytes came in. */
+    void NetworkReader::handleReceive(const boost::system::error_code & error, std::size_t bytes_transferred) {
+        LOG(INFO) << TAG << "Async recv finished code: " << error.value() << " " << error.message();
+        LOG_IF(WARNING, (error != boost::system::errc::success)) << "Receive failed in reader " << error.value();
+        if (!error || error == boost::asio::error::message_size)
+        {
+            if (buffer->data()) {
+                std::string buf;
+                buf.resize(bytes_transferred);
+                buf.assign(buffer->begin(), bytes_transferred);
+                LOG(INFO) << TAG << "Received " << bytes_transferred << " bytes of data: " << buf;
+                if (buf.length()>0) {
+                    nlohmann::json j = nlohmann::json::parse(buf);
+                    Package p = j.get<OHARBase::Package>();
+                    guard.lock();
+                    msgQueue.push(p);
+                    guard.unlock();
+                    // And when data has been received, notify the observer.
+                    observer.receivedData();
+                }
+            } else {
+                LOG(WARNING) << TAG << "Async recv finished but NO data";
+            }
+            if (running)
+            {
+                start();
+            }
+        }
+    }
+    
+    /** Stops the reader by setting the running flag to false, effectively ending the thread
+     loop in the threadFunc(). */
+    void NetworkReader::stop() {
+        LOG(INFO) << TAG << "Stop the reader...";
+        if (running) {
+            running = false;
+            LOG(INFO) << TAG << "Shutting down the socket.";
+            socket->cancel();
+            socket->close();
+        }
+    }
+    
+    /** Allows another object to read the package object received from the network.
+     This method should be called by the NetworkReaderObserver only when it has been notified
+     that data has arrived.
+     @return The Package containing the data received from the previous ProcessorNode.
+     */
+    Package NetworkReader::read() {
+        LOG(INFO) << TAG << "Reading results from reader";
+        guard.lock();
+        Package result;
+        if (!msgQueue.empty()) {
+            result = msgQueue.front();
+            msgQueue.pop();
+        }
+        guard.unlock();
+        return result;
+    }
+    
+    
 } //namespace
--- BaseLayer/include/OHARBaseLayer/Package.h
+++ BaseLayer/include/OHARBaseLayer/Package.h
@@ -15,99 +15,100 @@
 
 #include <nlohmann/json.hpp>
 
+
 namespace OHARBase {
-	
-	// Forward declaration.
-	class DataItem;
-	
-	/**
-	 Package encapsulates the data send between the ProcessorNode objects as well
-	 as passing the data around within a ProcessorNode and the Handlers and readers/writers
-	 in the node.<p>
-	 Each package has a unique identifier which can be used to identify data packages.
-	 Package also has a type, indicating is the package containing actual data to be
-	 handled, or is the package containing a control message. Control messages influence
-	 on the way the Node behaves. These include e.g. shutting down the node.
-	 @todo Add a "corrupt" flat for packages that have bad content. These packages
-	 are just passed through the Filters, logging their presence, and they are not
-	 handled by any Handler.
-	 @author Antti Juustila
-	 */
-	class Package {
-	public:
-		/*! The type of the package. */
-		enum Type {
-			NoType,   /*!< The package has no type (yet); it is uninitialized. */
-			Control,  /*!< The package contains a control message. */
-			Data      /*!< The package contains actual application specific data. */
-		};
-
-		Package();
-		Package(const Package & p);
-		Package(Package && p);
-		Package(const boost::uuids::uuid & id);
-		Package(Type ptype, const std::string & data);
-		Package(const boost::uuids::uuid & id, Type ptype, const std::string & data);
-		virtual ~Package();
-		
-		const boost::uuids::uuid & getUuid() const;
-		void setUuid(const boost::uuids::uuid & id);
-		Type getType() const;
-		void setType(Type ptype);
-		const std::string & getData() const;
-		void setData(const std::string & d);
-		const DataItem * getDataItem() const;
-		DataItem * getDataItem();
-		void setDataItem(const DataItem * item);
-		
-		bool isEmpty() const;
-		const Package & operator = (const Package & p);
-		const Package & operator = (Package && p);
-		bool operator == (const Package & pkg) const;
-		bool operator == (const std::string & str) const;
-		
-		static const std::string & separator();
-//		bool parse(const std::string & buffer);
-
-		const std::string & getTypeAsString() const;
-      void setTypeFromString(const std::string & typeStr);
-
-	private:
-		/** The unique identifier for a package. Generated using the boost library support. */
-		boost::uuids::uuid uid;
-		/** Type of the data package. It is either Control or Data. Control packages are
-		 commands to both ProcessorNode and DataHandler objects on how they are controlled.
-		 Commands are:<br />
-		 <ul><li>ping, which causes the Node to print out a ping command,</li>
-		 <li>readfile, which causes the Node to read it's data file if it has been configured to do so,</li>
-		 <li>shutdown, which causes the node to shut down itself in a controlled way.</li></ul>
-		 All the commands are also forwarded to the next Node in the Pipes & Filters architecture.
-		 Thus all Nodes can be shut down by writing the command "shutdown" in the console of the first Node.<p>
-		 Data packages are application specific data items. */
-		Type type;
-		/** Data as received from the network/sent to the network.*/
-		std::string data;
-		/** Parsed data, structure of which is application specific. Application developers
-		 subclass their data objects from DataItem and implement application specific data
-		 structures in their subclasses. Parsing of data from Package::data to Package::dataItem
-		 happens in other applicatin specific classes.<br />
-		 This member is null if nothing has been parsed, and not null if the pointer points to
-		 a data item parsed from input (either from network or from file, for example).*/
-		DataItem *dataItem;
-		
-		/** The character separator used to separate the data items in the string
-		 representation of the data. Used when data is received as string and when
-		 data is sent to the next Node. */
-		static const std::string separatorStr;
-		/** Textual representation of the package type Package::Control. */
-		static const std::string controlStr;
-		/** Textual representation of the package type Package::Data. */
-		static const std::string dataStr;
-	};
-	
-   void to_json(nlohmann::json & j, const Package & package);
-   void from_json(const nlohmann::json & j, Package & package);
-   
-   
+    
+    // Forward declaration.
+    class DataItem;
+    
+    /**
+     Package encapsulates the data send between the ProcessorNode objects as well
+     as passing the data around within a ProcessorNode and the Handlers and readers/writers
+     in the node.<p>
+     Each package has a unique identifier which can be used to identify data packages.
+     Package also has a type, indicating is the package containing actual data to be
+     handled, or is the package containing a control message. Control messages influence
+     on the way the Node behaves. These include e.g. shutting down the node.
+     @todo Add a "corrupt" flat for packages that have bad content. These packages
+     are just passed through the Filters, logging their presence, and they are not
+     handled by any Handler.
+     @author Antti Juustila
+     */
+    class Package {
+    public:
+        /*! The type of the package. */
+        enum Type {
+            NoType,   /*!< The package has no type (yet); it is uninitialized. */
+            Control,  /*!< The package contains a control message. */
+            Data      /*!< The package contains actual application specific data. */
+        };
+        
+        Package();
+        Package(const Package & p);
+        Package(Package && p);
+        Package(const boost::uuids::uuid & id);
+        Package(Type ptype, const std::string & data);
+        Package(const boost::uuids::uuid & id, Type ptype, const std::string & data);
+        virtual ~Package();
+        
+        const boost::uuids::uuid & getUuid() const;
+        void setUuid(const boost::uuids::uuid & id);
+        Type getType() const;
+        void setType(Type ptype);
+        const std::string & getData() const;
+        void setData(const std::string & d);
+        const DataItem * getDataItem() const;
+        DataItem * getDataItem();
+        void setDataItem(const DataItem * item);
+        
+        bool isEmpty() const;
+        const Package & operator = (const Package & p);
+        const Package & operator = (Package && p);
+        bool operator == (const Package & pkg) const;
+        bool operator == (const std::string & str) const;
+        
+        static const std::string & separator();
+        //		bool parse(const std::string & buffer);
+        
+        const std::string & getTypeAsString() const;
+        void setTypeFromString(const std::string & typeStr);
+        
+    private:
+        /** The unique identifier for a package. Generated using the boost library support. */
+        boost::uuids::uuid uid;
+        /** Type of the data package. It is either Control or Data. Control packages are
+         commands to both ProcessorNode and DataHandler objects on how they are controlled.
+         Commands are:<br />
+         <ul><li>ping, which causes the Node to print out a ping command,</li>
+         <li>readfile, which causes the Node to read it's data file if it has been configured to do so,</li>
+         <li>shutdown, which causes the node to shut down itself in a controlled way.</li></ul>
+         All the commands are also forwarded to the next Node in the Pipes & Filters architecture.
+         Thus all Nodes can be shut down by writing the command "shutdown" in the console of the first Node.<p>
+         Data packages are application specific data items. */
+        Type type;
+        /** Data as received from the network/sent to the network.*/
+        std::string data;
+        /** Parsed data, structure of which is application specific. Application developers
+         subclass their data objects from DataItem and implement application specific data
+         structures in their subclasses. Parsing of data from Package::data to Package::dataItem
+         happens in other applicatin specific classes.<br />
+         This member is null if nothing has been parsed, and not null if the pointer points to
+         a data item parsed from input (either from network or from file, for example).*/
+        DataItem *dataItem;
+        
+        /** The character separator used to separate the data items in the string
+         representation of the data. Used when data is received as string and when
+         data is sent to the next Node. */
+        static const std::string separatorStr;
+        /** Textual representation of the package type Package::Control. */
+        static const std::string controlStr;
+        /** Textual representation of the package type Package::Data. */
+        static const std::string dataStr;
+    };
+    
+    void to_json(nlohmann::json & j, const Package & package);
+    void from_json(const nlohmann::json & j, Package & package);
+    
+    
 } //namespace
 #endif /* defined(__PipesAndFiltersStudentNode__Package__) */
