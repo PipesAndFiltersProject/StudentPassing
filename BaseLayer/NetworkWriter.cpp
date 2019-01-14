24351a6d8d4755f6686111dd642b307487a0d681
Antti Juustila <antti.juustila@gmail.com>
2019-01-14 15:34:17 +0200

    Fixed initialization order of members due to a warning.

--- BaseLayer/NetworkWriter.cpp
+++ BaseLayer/NetworkWriter.cpp
@@ -30,7 +30,7 @@ namespace OHARBase {
      @param io_s The boost asio io service.
      */
     NetworkWriter::NetworkWriter(const std::string & hostName, boost::asio::io_service & io_s)
-    : Networker(hostName,io_s), TAG("NetWriter "), threader(nullptr)
+    : Networker(hostName,io_s), threader(nullptr), TAG("NetWriter ")
     {
         socket = std::unique_ptr<boost::asio::ip::udp::socket>(new boost::asio::ip::udp::socket(io_s));
     }
@@ -43,7 +43,7 @@ namespace OHARBase {
      @param io_s The boost asio io service.
      */
     NetworkWriter::NetworkWriter(const std::string & hostName, int portNumber, boost::asio::io_service & io_s)
-    : Networker(hostName, portNumber, io_s), TAG("NetWriter "), threader(nullptr)
+    : Networker(hostName, portNumber, io_s), threader(nullptr), TAG("NetWriter ")
     {
         socket = std::unique_ptr<boost::asio::ip::udp::socket>(new boost::asio::ip::udp::socket(io_s));
     }
